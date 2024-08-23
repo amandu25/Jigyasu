d33d5ca364258476fac235f021ef2496c4e384e4
Aman <amandubey918@gmail.com>
2024-08-23 19:56:50 +0530

    -

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,12 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt";
-import prisma from "../../../lib/prisma"; // Import the Prisma instance from your project
-import jwt from "jsonwebtoken"; // Import jsonwebtoken
+import prisma from "../../../lib/prisma";
+import jwt from "jsonwebtoken";
 
-// Define salt rounds for hashing (if needed elsewhere)
 const SALT_ROUNDS = 10;
 
-// Secret key for JWT (store this securely, e.g., in environment variables)
 const JWT_SECRET = process.env.JWT_SECRET || "jigyasuToken";
 
 export async function POST(req: NextRequest) {
@@ -23,7 +21,6 @@ export async function POST(req: NextRequest) {
 
     console.log("Received POST request with:", { username, password });
 
-    // Fetch the user with the provided username using Prisma
     const user = await prisma.userCredentials.findUnique({
       where: { username },
       select: { password: true },
@@ -40,19 +37,16 @@ export async function POST(req: NextRequest) {
 
     const storedHashedPassword = user.password;
 
-    // Compare the provided password with the stored hashed password
     const isMatch = await bcrypt.compare(password, storedHashedPassword);
 
     if (isMatch) {
-      // Generate JWT token
       const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: "1h" });
 
-      // Set the token in cookies
       const response = NextResponse.json({ message: "Login successful" });
       response.cookies.set("authToken", token, {
-        httpOnly: true, // Ensures the cookie is only accessible by the web server
-        secure: process.env.NODE_ENV === "production", // Ensures the cookie is sent over HTTPS
-        maxAge: 60 * 60, // 1 hour
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 60 * 60,
         path: "/",
       });
 
